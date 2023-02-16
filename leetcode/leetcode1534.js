f0a3ec4046fb5adab52548b92e1689e41736d862
Jonathan Paugh <j.d.paugh@gmail.com>
2023-02-15 18:47:50 -0800

    Add leetcode 1534 solution

---
+++ leetcode/leetcode1534.js
@@ -0,0 +1,40 @@
+/**
+ * Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+ *
+ * A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+ *
+ * 0 <= i < j < k < arr.length
+ * |arr[i] - arr[j]| <= a
+ * |arr[j] - arr[k]| <= b
+ * |arr[i] - arr[k]| <= c
+ * Where |x| denotes the absolute value of x.
+ *
+ * Return the number of good triplets.
+ */
+
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+let countGoodTriplets = function(arr, a, b, c) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (Math.abs(arr[i] - arr[j]) > a
+          || Math.abs(arr[j] - arr[k]) > b
+          || Math.abs(arr[i] - arr[k]) > c
+        ) {
+          continue;
+        }
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
